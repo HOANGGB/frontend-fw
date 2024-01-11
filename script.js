4a006b330914f5615aaaff2a4e2460af99a02189
hoang <hoang@gmail.com>
2024-01-11 21:31:48 +0700

    ALL CODE LAB 2

---
+++ script.js
@@ -0,0 +1,16 @@
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
